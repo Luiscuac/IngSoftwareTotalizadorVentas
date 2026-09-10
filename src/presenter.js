cb59024c1b395452b093b0f59d2d9c21c407cc87
Luiscuac <luiguiandre2208@gmail.com>
2026-09-09 21:59:51 -0400

    feat: integrar calculo de impuesto por categoria en la presentacion de resultados

--- src/presenter.js
+++ src/presenter.js
@@ -14,18 +14,24 @@ form.addEventListener("submit", (event) => {
   const precio = Number.parseFloat(precioInput.value);
   const estado = estadoInput.value.toUpperCase();
   const categoria = categoriaInput.value;
+  
   const neto = totalizador.calcularNeto(cantidad, precio);
+
   const descuento = totalizador.calcularDescuento(neto);
-  const precioConDescuento = neto - descuento;
-  const impuesto = totalizador.calcularImpuesto(precioConDescuento, estado);
   const descuentoCategoriaPorcentaje = totalizador.obtenerDescuentoPorCategoria(categoria);
   const descuentoCategoriaMonto = neto * descuentoCategoriaPorcentaje;
+  const precioConDescuentos = neto - descuento - descuentoCategoriaMonto;
+
+  const impuesto = totalizador.calcularImpuesto(precioConDescuentos, estado);
+  const impuestoCategoriaPorcentaje = totalizador.obtenerImpuestoPorCategoria(categoria);
+  const impuestoCategoriaMonto = precioConDescuentos * impuestoCategoriaPorcentaje;
   
   div.innerHTML = `
-    <p>Precio neto: $${neto}</p>
-    <p>Descuento general: $${descuento}</p>
-    <p>Descuento adicional (${categoria}): $${descuentoCategoriaMonto}</p>
-    <p>Impuesto (${estado}): $${impuesto}</p>
-    <h3>Total: $${(neto - descuento - descuentoCategoriaMonto) + impuesto}</h3>
+      <p>Precio neto: $${neto}</p>
+      <p>Descuento general: $${descuento}</p>
+      <p>Descuento adicional (${categoria}): $${descuentoCategoriaMonto}</p>
+      <p>Impuesto (${estado}): $${impuesto}</p>
+      <p>Impuesto adicional (${categoria}): $${impuestoCategoriaMonto}</p>
+      <h3>Total: $${(precioConDescuentos + impuesto + impuestoCategoriaMonto)}</h3>
   `;
 });
